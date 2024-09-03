7a56e863d15e37098da186c228824e12b6897760
Nimira43 <n.rabone72@aol.com>
2024-09-03 02:53:59 +0100

    Setting up basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log('Server')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
