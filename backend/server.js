2ebb30683f27e2b00dd8815c9faf5a4942a80cd6
Nimira43 <n.rabone72@aol.com>
2024-09-03 03:04:13 +0100

    Testedserver with the help of Postman

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,8 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'This is the Maintenance Ticket App'})
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
